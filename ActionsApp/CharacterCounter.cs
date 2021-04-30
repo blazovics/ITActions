2424bc62ca5fd31ee0be8c5d5a69859f0c873b35
Laszlo Blazovics <blazovics.laszlo@aut.bme.hu>
2021-04-30 10:42:00 +0200

    Project Ready

---
+++ ActionsApp/CharacterCounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActionsApp
+{
+    public class CharacterCounter
+    {
+        public int Characters(string str)
+        {
+            return str.Length;
+        }
+    }
+}
